8d57aff77cc4448ece3e572776af97611ab0b177
Travis Clark <travis6clark@gmail.com>
2019-12-31 11:25:22 -0800

    frontend container / component build out

---
+++ client/components/AddTipModal.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const AddTipModal = (props) => (
+  
+  <div className="addModal">
+    <p>Here We Can</p>
+    <p>Add a Tip</p>
+  </div>
+
+);
+
+export default AddTipModal;
---
+++ client/components/Search.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Search = (props) => (
+  
+  <div id="search" className="division">
+    <input type="text" placeholder="Zip Code"></input>
+    <button>ADD</button>
+  </div>
+
+);
+
+export default Search;
