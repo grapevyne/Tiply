cef988da1304b6a420d34ef10af324808f8cf1a9
Connie <clai011@ucr.edu>
2020-01-01 15:07:38 -0800

    Added Google fonts, Fontawesome, and more styling for icons

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 const Search = (props) => (
 
   <div id="search" className="division">
-    <input type="text" placeholder="Zip Code" onChange={props.getZipCode} className="search-bar" />
-    <button type="button" onClick={props.getLocalTips} className="search-button">Search</button>
-    <button type="button" className="add-button">ADD</button>
+    <input type="text" placeholder="Enter 5-digit Zip Code" onChange={props.getZipCode} className="search-bar" />
+    <button type="button" onClick={props.getLocalTips} className="search-button"><i class="fas fa-search-location"></i>Search</button>
+    <button type="button" className="add-button"><i class="fas fa-plus"></i> Tip</button>
   </div>
 
 );
