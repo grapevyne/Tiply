97662e8c3a70c9b784248dec28b1ac2e0a79e19d
Connie <clai011@ucr.edu>
2020-01-01 14:25:16 -0800

    Added classNames for buttons and input tag in Search.jsx

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 const Search = (props) => (
-  
+
   <div id="search" className="division">
-    <input type="text" placeholder="Zip Code" onChange={props.getZipCode} />
-    <button type="button" onClick={props.getLocalTips}>Search</button>
-    <button type="button">ADD</button>
+    <input type="text" placeholder="Zip Code" onChange={props.getZipCode} className="search-bar" />
+    <button type="button" onClick={props.getLocalTips} className="search-button">Search</button>
+    <button type="button" className="add-button">ADD</button>
   </div>
 
 );
