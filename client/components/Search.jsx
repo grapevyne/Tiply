05de93d1878d23df4770d05fef22c3260e14e083
Connie <clai011@ucr.edu>
2020-01-01 22:55:40 -0800

    modal styling needs improvement

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -5,12 +5,11 @@ const Search = props => {
   return (
     <div id="search" className="division">
       <input type="text" placeholder="Zip Code" onChange={props.getZipCode} />
-      <button type="button" onClick={() => props.getLocalTips(props.zipCode)}> Enter </button>
-      <input
-        type="button"
-        value="Add a Tip"
-        onClick={props.toggleAddTipsButton}
-      />
+      <button onClick={() => props.getLocalTips(props.zipCode)}> Enter </button>
+      <button
+        onClick={props.toggleAddTipsButton}>
+        Add a Tip
+      </button>
       {props.addTipsBoolean ? <AddTipModal /> : null}
     </div>
   )
