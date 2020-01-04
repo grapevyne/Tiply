75b8fc42b21c09cd433446ff401dbab3074e2ed0
Andrew Shin <andyrpg@gmail.com>
2020-01-04 09:18:32 -0800

    added server side caching

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const mcache = require('memory-cache');
+
 const path = require('path');
 
 const app = express();
@@ -16,7 +18,25 @@ const tipsRouter = require('./routes/routes');
 
 app.use('/tips', tipsRouter);
 
-app.get('/', (req, res) => {
+let cache = duration => {
+  return (req, res, next) => {
+      let key = '_express_' + req.originalUrl || req.originalUrl
+      let cachedBody = mcache.get(key);
+      if (cachedBody) {
+          res.send(cachedBody);
+          return
+      } else { 
+          res.sendResponse = res.send
+          res.send = (body) => { 
+              mcache.put(key, body, duration * 1000);
+              res.sendResponse(body)
+          }
+          next()
+      }
+  }
+}
+
+app.get('/', cache(10), (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
